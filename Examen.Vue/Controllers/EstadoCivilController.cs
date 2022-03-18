7825db095ab457e24ac64039fdffc4ad1b94ed66
Mario Escalante Cantu <ma.escalante@mavi.mx>
2022-03-18 14:29:55 -0600

    Add: EstadoCivil controller

---
+++ Examen.Vue/Controllers/EstadoCivilController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Examen.Core.COMMON.Interfaces;
+using Examen.Core.COMMON.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Examen.Vue.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstadoCivilController : ControllerBase
+    {
+        private readonly IEstadoCivilManager _estadoCivilManager;
+        public EstadoCivilController(IEstadoCivilManager estadoCivilManager)
+        {
+            _estadoCivilManager = estadoCivilManager;
+        }
+        // GET: api/EstadoCivil
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_estadoCivilManager.ObtenerTodos(new SpParametros("select * from EstadoCivil")));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET: api/EstadoCivil/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                return Ok(_estadoCivilManager.ObtenerTodos(new SpParametros($"select * from EstadoCivil where IdEstadoCivil = {id}")));
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // POST: api/EstadoCivil
+        [HttpPost]
+        public IActionResult Post(EstadoCivil value)
+        {
+            try
+            {
+                return Ok(_estadoCivilManager.Crear(new SpParametros($"insert into EstadoCivil(Nombre) values({value.Nombre})")));
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT: api/EstadoCivil/5
+        [HttpPut]
+        public IActionResult Put(EstadoCivil value)
+        {
+            try
+            {
+                return Ok(_estadoCivilManager.Actualizar(new SpParametros($"update EstadoCivil set Nombre = {value.Nombre} where IdEstadoCivil = {value.IdEstadoCivil}")));
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete]
+        public IActionResult Delete(List<int> ids)
+        {
+            try
+            {
+                string idsToDelete = "";
+                ids.ForEach(item => 
+                {
+                    idsToDelete += $"{item},";
+                });
+                return Ok(_estadoCivilManager.Actualizar(new SpParametros($"delete from EstadoCivil where IdEstadoCivil IN ({idsToDelete.Substring(0,idsToDelete.Length-1)})")));
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
