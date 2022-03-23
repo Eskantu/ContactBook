0a7c9c37d211e99a3166a14459beb67743c944b5
Mario Escalante Cantu <ma.escalante@mavi.mx>
2022-03-23 12:05:17 -0600

    Salto de linea para compilacion

--- UI/Web/Examen.Vue/Startup.cs
+++ UI/Web/Examen.Vue/Startup.cs
@@ -50,6 +50,7 @@ namespace Examen.Vue
                 app.UseDeveloperExceptionPage();
             }
 
+
             app.UseRouting();
             app.UseSpaStaticFiles();
             app.UseAuthorization();
