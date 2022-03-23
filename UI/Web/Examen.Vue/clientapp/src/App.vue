feab3b78171e776c932aa6cdb73f853b557982b4
Mario Escalante Cantu <ma.escalante@mavi.mx>
2022-03-23 17:31:14 -0600

    Modify: style

--- UI/Web/Examen.Vue/clientapp/src/App.vue
+++ UI/Web/Examen.Vue/clientapp/src/App.vue
@@ -2,45 +2,88 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
       dark
+      clipped-left
+      clipped-right
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+      @click="drawer = !drawer"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="../src/assets/logo.png"
-          transition="scale-transition"
-          width="40"
-        />
-      </div>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/Eskantu/ContactBook"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Git</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>Contact Book</v-toolbar-title>
     </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      stateless
+      hide-overlay
+      :width="300"
+      dark
+      app
+    >
+      <v-list dense>
+        <v-list-item-group>
+          <v-list-item link  :to="{ name: 'Home' }">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
 
+                    <v-list-item>
+            <v-list-item-icon>
+              <v-icon>contact_page</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Contactos</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>format_list_bulleted</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Tipo contacto</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>favorite</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Estado civil</v-list-item-title>
+          </v-list-item>
+          <v-list-item link :to="{ name: 'About' }">
+            <v-list-item-icon>
+              <v-icon>question_mark</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+       <template v-slot:append>
+        <div class="pa-2">
+          <v-btn color='deep-purple accent-4' block>
+            Salir
+          </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
+    <v-footer padless dark>
+      <v-col class="text-center" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Contact-Book</strong>
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
-    //
+    drawer: false,
   }),
 };
 </script>
--- UI/Web/Examen.Vue/clientapp/src/views/Home.vue
+++ UI/Web/Examen.Vue/clientapp/src/views/Home.vue
@@ -1,8 +1,12 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Sitio en construcción</h1>
+  <v-container class="jumbotron-eskantu" fluid fill-height>
+    <v-row align="center">
+      <v-col offset-md="2" md="8" class="text-md-center">
+        <v-card>
+          <v-card-title class="white--text">
+            <h2>Bienvenido a Contact - Book</h2>
+          </v-card-title>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -14,3 +18,29 @@ export default {
   components: {},
 };
 </script>
+
+
+<style lang="scss" scoped>
+.jumbotron-eskantu {
+  background: linear-gradient(
+      to top right,
+      rgba(71, 51, 41, 0.7),
+      rgba(65, 94, 86, 0.7)
+    );
+  background-size: cover;
+  height: 100%;
+  .row {
+    height: 100% !important;
+  }
+  .v-card {
+    background-color: rgba(255, 255, 255, 0.4) !important;
+  }
+  .v-card__title {
+    word-break: break-word;
+
+    h2 {
+      margin: 0px auto;
+    }
+  }
+}
+</style>
