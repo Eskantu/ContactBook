1b3ce1f0cbeeecd9acfc07506b3e22e653681035
Mario Escalante Cantu <ma.escalante@mavi.mx>
2022-03-24 14:13:29 -0600

    add: configurations for eslint

--- UI/Web/Examen.Vue/clientapp/.eslintrc.js
+++ UI/Web/Examen.Vue/clientapp/.eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
     sourceType: 'module'
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  extends: [
+    'standard',
+    'plugin:vue/base',
+    'plugin:vuetify/base'
+  ],
   // We could also use the https://github.com/vuejs/eslint-plugin-vue
   // required to lint *.vue files
   plugins: [
@@ -13,12 +17,11 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
-    'no-console': 0,
-    "semi": 0,
+    'no-console': ["error", { allow: ["warn", "error","log"] }],
+    'semi': 0,
+    //'accessor-pairs':0,
     // allow paren-less arrow functions
     'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
