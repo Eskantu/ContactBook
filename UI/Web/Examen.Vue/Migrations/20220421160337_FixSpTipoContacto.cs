6303afc5ecf5cb1006d0401ab7c165c343b1e729
Mario Escalante <a16021553@iteshu.edu.mx>
2022-04-21 11:09:36 -0500

    Add: migration to fix SpTipoContacto

---
+++ UI/Web/Examen.Vue/Migrations/20220421160337_FixSpTipoContacto.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Examen.Vue.Migrations
+{
+    public partial class FixSpTipoContacto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string spTipoContacto = @"  IF OBJECT_ID('SpTipoContacto')>0
+        DROP PROCEDURE SpTipoContacto;
+    GO
+
+    create procedure SpTipoContacto
+        @Opcion INT,
+        @Nombre VARCHAR(50) ='',
+        @IdTipoContacto INT =0
+    AS
+    BEGIN
+        IF(@Opcion=1)
+        BEGIN
+            INSERT INTO TipoContacto (Nombre) VALUES(@Nombre)
+        END
+        IF(@Opcion=2)
+        BEGIN
+           UPDATE TipoContacto SET Nombre=@Nombre WHERE IdTipoContacto=@IdTipoContacto
+        END
+        IF(@Opcion=3)
+        BEGIN
+            delete from TipoContacto WHERE IdTipoContacto=@IdTipoContacto
+
+        END
+        IF(@Opcion=4)
+        BEGIN
+            SELECT *
+            FROM TipoContacto
+        END
+    END";
+            migrationBuilder.Sql(spTipoContacto);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
