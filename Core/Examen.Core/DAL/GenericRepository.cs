1fcc761cbfefff52674f4d432baa765415399cb8
Mario Escalante <a16021553@iteshu.edu.mx>
2022-04-21 12:50:37 -0500

    refactory: add format to class and add DateTime format to SQLCommand

--- Core/Examen.Core/DAL/GenericRepository.cs
+++ Core/Examen.Core/DAL/GenericRepository.cs
@@ -11,142 +11,142 @@ using System.Threading.Tasks;
 
 namespace Examen.Core.DAL
 {
-    public class GenericRepository<T> : IGenericRepository<T> where T : class
+  public class GenericRepository<T> : IGenericRepository<T> where T : class
+  {
+    private readonly IConnection _connection;
+    public GenericRepository(IConnection connection)
     {
-        private readonly IConnection _connection;
-        public GenericRepository(IConnection connection)
-        {
-            _connection = connection;
+      _connection = connection;
 
-        }
-
-        public string Error { get; private set; }
-
-        public bool Create(SpParametros parametros)
-        {
-            bool result;
-            try
-            {
-                result = _connection.ExecuteNoQuery(CrearComando(parametros)) > 0;
-            }
-            catch (Exception ex)
-            {
-                Error = ex.Message;
-                result = false;
-            }
-            return result;
-        }
-
-
-
-        public bool Delete(SpParametros parametros)
-        {
-            bool result = false;
-            try
-            {
-                result = _connection.ExecuteNoQuery(CrearComando(parametros)) > 0;
-            }
-            catch (Exception ex)
-            {
-                Error = ex.Message;
-
-            }
-            return result;
-        }
-
-        public bool Update(SpParametros parametros)
-        {
-            bool result = false;
-            try
-            {
-                result = _connection.ExecuteNoQuery(CrearComando(parametros)) > 0;
-            }
-            catch (Exception ex)
-            {
-                Error = ex.Message;
-            }
-            return result;
-        }
-
-        private string CrearComando(SpParametros parametros)
-        {
-            string comando = $"{parametros.Nombre} ";
-            parametros.Parametros.ForEach(item =>
-            {
-                comando += $"{item.Key}={formatValue(item.Value)},";
-            });
-            return comando[0..^1];
-        }
-
-        private object formatValue(object value)
-        {
-            object valueFormated=value;
-            string nameMember = value.GetType().GetTypeInfo().Name;
-            if (nameMember == "String")
-            {
-                valueFormated = @$"'{value}'";
-            }
-            if (nameMember == "Int")
-            {
-                valueFormated = @$"{value}";
-            }
-            //if (nameMember == "DateTime")
-            //{
-            //    DateTime date = value as DateTime;
-            //    valueFormated = @$"'{}'";
-            //}
-            return valueFormated;
-        }
-
-        public List<T> Read(SpParametros parametros)
-        {
-            List<T> data;
-            using (IDataReader reader = _connection.ExecuteQuery(CrearComando(parametros)) as IDataReader)
-            {
-                data = reader.Select(r => CrearEntidad(r, typeof(T))).ToList();
-                reader.Close();
-            }
-            return data;
-        }
-
-        public List<Y> Query<Y>(SpParametros parametros)
-        {
-            List<Y> data;
-            using (IDataReader reader = _connection.ExecuteQuery(CrearComando(parametros)) as IDataReader)
-            {
-                data = reader.Select(r => CrearModelo<Y>(r, typeof(Y))).ToList();
-                reader.Close();
-            }
-            return data;
-        }
-
-        private T CrearEntidad(IDataReader data, Type tipoDato)
-        {
-            T entidad;
-            PropertyInfo[] properties = tipoDato.GetProperties();
-            entidad = (T)Activator.CreateInstance(tipoDato);
-            properties.ToList().ForEach(property =>
-            {
-                property.SetValue(entidad, data[property.Name] == DBNull.Value ? null : data[property.Name]);
-            });
-            return entidad;
-        }
-        private Y CrearModelo<Y>(IDataReader data, Type tipoDato)
-        {
-            Y entidad;
-            PropertyInfo[] properties = tipoDato.GetProperties();
-            entidad = (Y)Activator.CreateInstance(tipoDato);
-            properties.ToList().ForEach(property =>
-            {
-                property.SetValue(entidad, data[property.Name] == DBNull.Value ? null : data[property.Name]);
-            });
-            return entidad;
-        }
-
-        ~GenericRepository()
-        {
-            if (_connection.Disconnect() == false)
-                throw new Exception("Error al desconectar con servidor");
-        }
     }
+
+    public string Error { get; private set; }
+
+    public bool Create(SpParametros parametros)
+    {
+      bool result;
+      try
+      {
+        result = _connection.ExecuteNoQuery(CrearComando(parametros)) > 0;
+      }
+      catch (Exception ex)
+      {
+        Error = ex.Message;
+        result = false;
+      }
+      return result;
+    }
+
+
+
+    public bool Delete(SpParametros parametros)
+    {
+      bool result = false;
+      try
+      {
+        result = _connection.ExecuteNoQuery(CrearComando(parametros)) > 0;
+      }
+      catch (Exception ex)
+      {
+        Error = ex.Message;
+
+      }
+      return result;
+    }
+
+    public bool Update(SpParametros parametros)
+    {
+      bool result = false;
+      try
+      {
+        result = _connection.ExecuteNoQuery(CrearComando(parametros)) > 0;
+      }
+      catch (Exception ex)
+      {
+        Error = ex.Message;
+      }
+      return result;
+    }
+
+    private string CrearComando(SpParametros parametros)
+    {
+      string comando = $"{parametros.Nombre} ";
+      parametros.Parametros.ForEach(item =>
+      {
+        comando += $"{item.Key}={formatValue(item.Value)},";
+      });
+      return comando[0..^1];
+    }
+
+    private object formatValue(object value)
+    {
+      object valueFormated = value;
+      string nameMember = value.GetType().GetTypeInfo().Name;
+      if (nameMember == "String")
+      {
+        valueFormated = @$"'{value}'";
+      }
+      if (nameMember == "Int")
+      {
+        valueFormated = @$"{value}";
+      }
+      if (nameMember == "DateTime")
+      {
+        DateTime date = Convert.ToDateTime(value);
+        valueFormated = @$"'{date.ToString("yyyy/MM/dd")}'";
+      }
+      return valueFormated;
+    }
+
+    public List<T> Read(SpParametros parametros)
+    {
+      List<T> data;
+      using (IDataReader reader = _connection.ExecuteQuery(CrearComando(parametros)) as IDataReader)
+      {
+        data = reader.Select(r => CrearEntidad(r, typeof(T))).ToList();
+        reader.Close();
+      }
+      return data;
+    }
+
+    public List<Y> Query<Y>(SpParametros parametros)
+    {
+      List<Y> data;
+      using (IDataReader reader = _connection.ExecuteQuery(CrearComando(parametros)) as IDataReader)
+      {
+        data = reader.Select(r => CrearModelo<Y>(r, typeof(Y))).ToList();
+        reader.Close();
+      }
+      return data;
+    }
+
+    private T CrearEntidad(IDataReader data, Type tipoDato)
+    {
+      T entidad;
+      PropertyInfo[] properties = tipoDato.GetProperties();
+      entidad = (T)Activator.CreateInstance(tipoDato);
+      properties.ToList().ForEach(property =>
+      {
+        property.SetValue(entidad, data[property.Name] == DBNull.Value ? null : data[property.Name]);
+      });
+      return entidad;
+    }
+    private Y CrearModelo<Y>(IDataReader data, Type tipoDato)
+    {
+      Y entidad;
+      PropertyInfo[] properties = tipoDato.GetProperties();
+      entidad = (Y)Activator.CreateInstance(tipoDato);
+      properties.ToList().ForEach(property =>
+      {
+        property.SetValue(entidad, data[property.Name] == DBNull.Value ? null : data[property.Name]);
+      });
+      return entidad;
+    }
+
+    ~GenericRepository()
+    {
+      if (_connection.Disconnect() == false)
+        throw new Exception("Error al desconectar con servidor");
+    }
+  }
 }
