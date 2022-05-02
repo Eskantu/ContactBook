964f4ec08cda00ba4a28dc11c58b29435a4d61b7
Mario Escalante <a16021553@iteshu.edu.mx>
2022-05-02 13:35:34 -0500

    change connection string

--- UI/Web/Examen.Vue/ServicesCollectionExtensions.cs
+++ UI/Web/Examen.Vue/ServicesCollectionExtensions.cs
@@ -14,11 +14,11 @@ namespace Examen.Vue
     {
         public static void AddManagers(this IServiceCollection services, IConfiguration configuration)
         {
-#if DEBUG
-            string connectionString = configuration.GetConnectionString("DevlopConnection");
-#else
+//#if DEBUG
+//            string connectionString = configuration.GetConnectionString("DevlopConnection");
+//#else
+//#endif
             string connectionString = configuration.GetConnectionString("ProductionConnection");
-#endif
 
 
             FactoryManager factoryManager = new FactoryManager(connectionString);
