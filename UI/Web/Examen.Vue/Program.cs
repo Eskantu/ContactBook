c505938d78d7a06166ac9761d8cbffbe402f8651
Mario Escalante <a16021553@iteshu.edu.mx>
2022-05-10 18:08:42 -0500

    Add Serialog to create logs of app

--- UI/Web/Examen.Vue/Program.cs
+++ UI/Web/Examen.Vue/Program.cs
@@ -10,21 +10,43 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
 
 namespace Examen.Vue
 {
-    public class Program
+  public class Program
+  {
+    [Obsolete]
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+      IConfigurationRoot configuration = new ConfigurationBuilder()
+      .AddJsonFile("appsettings.json", false, true)
+      .Build();
+      #if DEBUG
+      string connectionString = configuration.GetConnectionString("DevlopConnection");
+#else
+      string connectionString = configuration.GetConnectionString("ProductionConnection");
+#endif
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      Log.Logger = new LoggerConfiguration()
+      .WriteTo.
+      MSSqlServer(connectionString, "Logs", autoCreateSqlTable: true, 
+      restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose)
+      .CreateLogger();
+
+
+
+      CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>()
+              .UseSerilog();
+
+            });
+  }
 }
